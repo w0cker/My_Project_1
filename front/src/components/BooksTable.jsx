741889ee97041acb2b11e8c53044903a776b7280
Alex Pustilnikov <58172002+w0cker@users.noreply.github.com>
2025-11-15 17:47:44 +0200

    Rename table header from 'No :' to 'No_'

--- front/src/components/BooksTable.jsx
+++ front/src/components/BooksTable.jsx
@@ -8,7 +8,7 @@ const BooksTable = ({ books }) => {
     <table className='w-full border-separate border-spacing-2'>
       <thead>
         <tr>
-          <th className='border border-slate-600 rounded-md'>No :</th>
+          <th className='border border-slate-600 rounded-md'>No_:</th>
           <th className='border border-slate-600 rounded-md'>Title</th>
           <th className='border border-slate-600 rounded-md'>Author</th>
           <th className='border border-slate-600 rounded-md'>Year</th>
